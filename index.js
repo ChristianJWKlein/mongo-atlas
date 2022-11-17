fc14754a01a0ff734122f794eaa85842abcd3d95
Christian JW Klein <cklein010715@gmail.com>
2022-11-16 23:12:22 -0500

    db basics brush up

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-//API Express
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
@@ -11,7 +10,7 @@ app.listen(3000, () => {
   console.log("listening on 3000");
 });
 
-const mongodb = require("mongodb"); //importing code from the node modules to the left (other developers code for mongodb)
+const mongodb = require("mongodb");
 const client = new mongodb.MongoClient(process.env.MONGO_URL);
 
 const connectClient = async () => {
@@ -27,11 +26,11 @@ const getUserCollection = () => {
   return col;
 };
 
-// const getProductCollection = () => {
-//   const db = client.db("christians-ecom-db");
-//   const col = db.collection("products");
-//   return col;
-// };
+const getProductCollection = () => {
+  const db = client.db("christians-ecom-db");
+  const col = db.collection("products");
+  return col;
+};
 
 const getUsers = async () => {
   const col = getUserCollection();
@@ -47,10 +46,29 @@ app.post("/insertuser", async (req, res) => {
   const user = req.body;
   if (!user.name || !user.address) {
     res.send("Name or address not entered");
-    return; //IF RETURN NOT THERE YOU DOnt prevent from happeniing
+    return; //IF RETURN NOT THERE YOU DON'T PREVENT UNFORMATTED INFO from entering DB
   }
   await insertUser(user);
-  res.send(`succesfully inserted user: ${JSON.stringify(user)}`); //let
+  res.send(`succesfully inserted user: ${JSON.stringify(user)}`);
+});
+
+app.get("/products", async (res, req) => {
+  getProducts().then((products) => res.send(products));
+});
+
+app.post("/insertproduct", async (res, req) => {
+  const product = req.body;
+  if (
+    !product.title ||
+    !product.description ||
+    !product.product ||
+    !product.price
+  ) {
+    res.send("Missing title, description, product, and/or price ");
+    return;
+  }
+  await insertProduct(product);
+  res.send(`succesfully inserted product: ${JSON.stringify(product)}`);
 });
 
 const insertUser = async (user) => {
@@ -59,17 +77,16 @@ const insertUser = async (user) => {
   console.log("User Inserted by user!");
 };
 
-// const insertProduct = async () => {
-//   const col = getProductCollection(product);
-//   await col.insertOne({
-//     product
-//   });
-//   console.log("Product added!");
-// };
+const insertProduct = async () => {
+  const col = getProductCollection(product);
+  await col.insertOne({
+    product,
+  });
+  console.log("Product added!");
+};
 
-// const getProducts = async () => {
-//   //this function gets the users in the user collection
-//   const col = getProductCollection();
-//   const products = await col.find({}).toArray(); //.find accepts a query this is asking for everything in the collection. then have to call .toArray bc it executes the query which is a promise. .find doesnt return a promise
-//   return products;
-// };
+const getProducts = async () => {
+  const col = getProductCollection();
+  const products = await col.find({}).toArray();
+  return products;
+};
